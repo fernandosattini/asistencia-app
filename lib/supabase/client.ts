a3ac46c779e54903c130b66bae80a3c2caa20b5e
fernandosattini <guiyelka2025@gmail.com>
2025-10-18 11:34:24 -0300

    Update client.ts
    intento de conexion con supabase

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,20 @@
 import { createBrowserClient } from "@supabase/ssr"
 
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  process.env.SUPABASE_SUPABASE_NEXT_PUBLIC_SUPABASE_URL;
+
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  process.env.SUPABASE_NEXT_PUBLIC_SUPABASE_ANON_KEY_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Faltan variables de entorno para Supabase");
+}
+
 export function createClient() {
   return createBrowserClient(
-    process.env.SUPABASE_SUPABASE_NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_NEXT_PUBLIC_SUPABASE_ANON_KEY_ANON_KEY!,
-  )
+    supabaseUrl,
+    supabaseAnonKey
+  );
 }
